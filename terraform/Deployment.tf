ad76b2222f619ae637112bd0f23c289e081f18db
Dilfuza Ibaidullaeva <47833854+dilfuza97@users.noreply.github.com>
2023-02-01 05:34:21 -0800

    Create Deployment.tf

---
+++ terraform/Deployment.tf
@@ -0,0 +1,45 @@
+resource "kubernetes_deployment" "java-app" {
+  metadata {
+    name = "java-app"
+    labels = {
+      app = "java-app"
+    }
+  }
+
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        app = "java-app"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "java-app"
+        }
+      }
+      spec {
+        container {
+          image = "sherqodirov/mywebapp:latest"
+          name  = "java-app"
+
+          port {
+            container_port = 8085
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
